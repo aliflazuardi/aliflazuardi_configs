9317263d5128895ee02f54dd3d1d7bc2fc85b74b
aliflazuardi <aliflazuardi250999@gmail.com>
2024-08-07 20:58:32 +0700

    add new colorscheme config for neovim

---
+++ vim/nvim/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "sho-87/kanagawa-paper.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-paper",
+    },
+  }
+}
