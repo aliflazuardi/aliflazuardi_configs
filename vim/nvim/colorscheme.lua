783dbfba80c8b20cd95f5ea554e69ac16b425102
aliflazuardi <aliflazuardi250999@gmail.com>
2024-09-08 20:26:51 +0700

    change nvim colorscheme to vscode theme

--- vim/nvim/colorscheme.lua
+++ vim/nvim/colorscheme.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    "sho-87/kanagawa-paper.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "kanagawa-paper",
-    },
-  }
+	{
+		"Mofiqul/vscode.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "vscode",
+		},
+	},
 }
